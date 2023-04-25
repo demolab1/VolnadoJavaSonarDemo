175e4d3d0fd02c8b7b34aad42d2fb0c554cba6a3
ComplianceLab <116727839+ComplianceLab@users.noreply.github.com>
2023-04-25 10:40:53 +0100

    Update main.tf

--- reverse_shell/main.tf
+++ reverse_shell/main.tf
@@ -25,15 +25,7 @@ resource "aws_route_table" "r" {
   }
 }
 
-resource "aws_subnet" "subnet" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "${var.subnet_cidr}"
-  availability_zone = "${var.region}b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "tmp_vulnado_rev_shell_subnet"
-  }
-}
+
 
 resource "aws_route_table_association" "assoc" {
   subnet_id      = "${aws_subnet.subnet.id}"
